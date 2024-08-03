18fe054b153ccf1b0e6c344639d9ecbf4bc14183
ASR <asrabbi0.com@gmail.com>
2024-08-03 20:25:04 +0600

    Second Commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import *
 from .serializers import *
 from .pagination import *
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -20,18 +21,46 @@ def index(request):
     }
     persons=[person1,person2]
     return Response(persons)
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def todo_list(request):
     todos=Todo.objects.all()
-    paginator=TodoPagination()
-    page=paginator.paginate_queryset(todos, request)
-    if page is not None:
+    if request.method == 'GET':
+        paginator=TodoPagination()
+        page=paginator.paginate_queryset(todos, request)
+        if page is not None:
+            serializer=TodoListSerializer(todos, many=True)
+            return paginator.get_paginated_response(serializer.data)
         serializer=TodoListSerializer(todos, many=True)
-        return paginator.get_paginated_response(serializer.data)
-    serializer=TodoListSerializer(todos, many=True)
-    return Response(serializer.data)
-@api_view(['GET'])
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        data=request.data
+        serializer=TodoCreateSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['GET','PATCH','DELETE'])
 def todo_detail(request, id):
     todo=get_object_or_404(Todo, id=id)
-    serializer=TodoDetailSerializer(todo)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer=TodoDetailSerializer(todo)
+        return Response(serializer.data)
+    
+    elif request.method == 'PATCH':
+        data=request.data
+        serializer=TodoDetailSerializer(todo, data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, sttaus=status.HTTP_400_BAD_REQUEST)
+   
+   
+    if request.method == 'DELETE':
+        todo.delete()
+        message={"success:Todo has been deleted successfully"}
+        return Response(message, status=status.HTTP_204_NO_CONTENT)
